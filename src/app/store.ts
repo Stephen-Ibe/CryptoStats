eaf90208a951e13fc49a420a0d4a9a74da91f59c
KalsonSaint <saintkalson@gmail.com>
2022-01-14 23:28:54 +0100

    authApi and reducer  setup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,3 +1,4 @@
+import { authApi } from "./../apis/auth.api";
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { usersApi } from "../apis/users.api";
 import counterReducer from "../features/counter/counterSlice";
@@ -5,10 +6,13 @@ import counterReducer from "../features/counter/counterSlice";
 export const store = configureStore({
   reducer: {
     [usersApi.reducerPath]: usersApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
     counter: counterReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(usersApi.middleware),
+    getDefaultMiddleware()
+      .concat(usersApi.middleware)
+      .concat(authApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
